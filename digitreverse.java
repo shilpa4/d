32caa7459f204a2274d4e597c3f2977f954fd7bc
shilpa4 <shilpasre91@gmail.com>
2016-07-21 17:19:29 +0530

    Add files via upload

---
+++ digitreverse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+	import java.util.Scanner;
+	public class digitreverse {
+
+		    public static void main(String [] args){
+		      int s1,a;
+		      Scanner fa=new Scanner(System.in);
+		      s1=fa.nextInt();
+		while(s1!=0){
+			a=s1%10;
+			System.out.print(a);
+			s1=s1/10;
+		}
+		    
+		}
+
+	}
+
+
