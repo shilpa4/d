a0cc7debcc699de40f018aef74e1f6e0222945e6
shilpa4 <shilpasre91@gmail.com>
2016-07-22 10:16:19 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class compare {
+	public static void main(String [ ] arg) {
+		String n;
+		   
+	    Scanner ss=new Scanner(System.in);
+	    n=ss.nextLine();
+	    String n1;
+		   
+	    Scanner as=new Scanner(System.in);
+	    n1=as.nextLine();
+	if(n.equals(n1)){
+		System.out.println("equal");
+	}
+	else
+	{
+		System.out.println("not equal");
+	}
+	
+	
+	}
+}
