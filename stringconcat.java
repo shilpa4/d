3c4479122d5455b398cbd7ea9c33d736ba48d737
shilpa4 <shilpasre91@gmail.com>
2016-07-22 09:47:34 +0530

    Add files via upload

---
+++ stringconcat.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class stringconcat {
+	 public static void main(String [] args){
+		    String n;
+		   
+		    Scanner ss=new Scanner(System.in);
+		    n=ss.nextLine();
+		    String n1;
+			   
+		    Scanner as=new Scanner(System.in);
+		    n1=as.nextLine();
+	 String s2=n+n1;
+	 
+	 System.out.println(s2);
+	 }
+}
