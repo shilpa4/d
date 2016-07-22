455552004bd679f6e53246839a8e261fec335c37
shilpa4 <shilpasre91@gmail.com>
2016-07-22 09:54:17 +0530

    Add files via upload

--- maxnum.java
+++ maxnum.java
@@ -8,14 +8,14 @@
 		public static void main(String [ ] arg) {
 			int a[]=new int[10];
 			Scanner name=new Scanner(System.in);
-		for(int i=0;i<10;i++)
+		for(int i=0;i<3;i++)
 		{
 			a[i]=name.nextInt();
 		}
 		
-			for(int j=0;j<10;j++)
+			for(int j=0;j<3;j++)
 		{
-				for(int k=j+1;k<10;k++)
+				for(int k=j+1;k<3;k++)
 				{
 				if(a[j]>a[k])
 				{
@@ -26,7 +26,7 @@
 				}
 			}
 			}
-		System.out.println(a[9]);
+		System.out.println(a[2]);
 			
 		
 			
