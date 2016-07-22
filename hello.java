8d91e46902b75ba6ef0112d0f0e5abf195b6189e
shilpa4 <shilpasre91@gmail.com>
2016-07-22 11:13:06 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+
+public class hello {
+    public static void main(String args[]) {
+    String fg="hello";
+    for(int i=0;i<5;i++){
+    	System.out.println("hello");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
